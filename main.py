7f331e5aeb2926cab72461644978ce04dd8fb34f
Yuma Ito <yumaito805@proton.me>
2026-09-01 08:53:55 -0700

    udpated:avatar

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from flask import Flask,render_template,request,redirect,url_for,session,flash
 from flask_sqlalchemy import SQLAlchemy
+import os
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {"png","jpg","jpeg","webp"}
+def allowed_file(filename):
+    return(
+        "." in filename
+        and filename.split(".",1)[1].lower() in ALLOWED_EXTENSIONS
+    )
+
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "change-this-secret-key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["UPLOAD_FOLDER"] = os.path.join(
+    app.root_path,
+    "static",
+    "uploads"
+)
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 db = SQLAlchemy(app)
 
@@ -14,6 +30,7 @@ class User(db.Model):
     name = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    avatar = db.Column(db.String(255), nullable=True)
 
 
 with app.app_context():
@@ -45,7 +62,8 @@ def loginAuth():
                 session['email'] = user.email
                 session['name'] = user.name
                 session['password'] = user.password
-                flash("Login success")
+                session['avatar'] = user.avatar
+                # flash("Login success")
                 return redirect(url_for("dashboard"))
         flash("Invalid email or password.")
 
@@ -91,6 +109,49 @@ def updateProfile():
             session['password'] = user.password
             flash("Profile updated successfully")
             return redirect(url_for("dashboard")) 
+    return render_template("dashboard.html")
+
+@app.route("/avatar",methods = ['POST'])
+def change_avatar():
+    user = User.query.filter_by(email = session['email']).first()
+        # print("user",user)
+    file = request.files['avatar']
+    if not allowed_file(file.filename):
+        flash("not allowed")
+        return redirect(url_for("dashboard"))
+    
+    
+    extesion = file.filename.split(".",1)[1].lower()
+    filename = f"user_{session['user_id']}.{extesion}"
+    filepath = os.path.join(
+        app.config['UPLOAD_FOLDER'],
+        filename
+    )
+    if user.avatar:
+        old_file = os.path.join(
+            app.config['UPLOAD_FOLDER'],
+            user.avatar
+        ) 
+        if os.path.exists(old_file):
+            os.remove(old_file)
+    file.save(filepath)
+    user.avatar = filename
+    session['avatar'] = user.avatar
+    db.session.commit()
+    flash("Upload success")
+    return redirect(url_for("dashboard"))
+
+@app.route("/avatar",methods = ['DELETE'])
+def remove_avatar():
+    session['avatar'] = None
+    user = User.query.filter_by(email = session['email']).first()
+    old_file = os.path.join(
+        app.config['UPLOAD_FOLDER'],user.avatar
+    )
+    if os.path.exists(old_file):
+        os.remove(old_file)
+    user.avatar = None
+    db.session.commit()
     return render_template("dashboard.html")
 
 if __name__ == "__main__":
